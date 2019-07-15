79df28477bbff08e95bee4615361f2fb8d181fd4
gabaaa <not-set>
2019-07-15 07:17:44 -0500

    Bump Release Version 4.0.1

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # author: Gabriel Auger
-# version: 4.0.0
+# version: 4.0.1
 # name: message
 # license: MIT
 
-__version__ = "4.0.0"
+__version__ = "4.0.1"
 
 from .dev.message import dbg, error, ft, info, success, warning
