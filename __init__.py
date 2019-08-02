ec54502ab997f9ad88ce99fd296be455687a5fcf
gabaaa <not-set>
2019-08-02 14:55:01 -0500

    Bump Release Version 5.0.5

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # author: Gabriel Auger
-# version: 5.0.4
+# version: 5.0.5
 # name: message
 # license: MIT
 
-__version__ = "5.0.4"
+__version__ = "5.0.5"
 
 from .dev.message import dbg, error, ft, info, success, warning
--- dev/message.py
+++ dev/message.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # author: Gabriel Auger
-# version: 5.0.4
+# version: 5.0.5
 # name: message
 # license: MIT
 
